dc8e30863a2624c84f1a4e933061ba2ee1b633d6
Daniel Veksler <danil.vexler123@gmail.com>
2025-09-25 15:28:06 -0400

    diff cors setting

--- app/__init__.py
+++ app/__init__.py
@@ -2,17 +2,17 @@ from fastapi import FastAPI
 from app.endpoints import users
 from fastapi.middleware.cors import CORSMiddleware
 
-cors_allowed_origins = [
-    "http://localhost",
-    "http://localhost:8000",
-    "https://retrorooms-chat-back.onrender.com/"
-]
+# cors_allowed_origins = [
+#     "http://localhost",
+#     "http://localhost:8000",
+#     "https://retrorooms-chat-back.onrender.com/"
+# ]
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=cors_allowed_origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
